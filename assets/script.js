50e0ef80fef500c4f73b8e41f329c78570134f80
Raphael Maia <style16@msn.com>
2023-07-28 20:09:00 -0300

    alterações 1

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle ('light')
   // pegar a tag img
 const img = document.querySelector('#profile img')
 // subustituir a img
 if (html.classList.contains('light')) {
 
-  img.setAttribute('src','./assets/rosana.png')}
+  img.setAttribute('src','./assets/rosana2.png')}
   else {
-    img.setAttribute ('src', './assets/rosanal.png')
+    img.setAttribute ('src', './assets/rosana1.png')
   }
 }
   
