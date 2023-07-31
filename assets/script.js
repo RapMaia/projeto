5d9d162b7ffb883f96c07ac5f0a1337c14ed5d7d
Raphael Maia <style16@msn.com>
2023-07-31 10:51:19 -0300

    prejeto rosana

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,9 @@ const img = document.querySelector('#profile img')
 // subustituir a img
 if (html.classList.contains('light')) {
 
-  img.setAttribute('src','./assets/flu1.png')}
+  img.setAttribute('src','./assets/rosana1.png')}
   else {
-    img.setAttribute ('src', './assets/brasil.png')
+    img.setAttribute ('src', './assets/rosana2.png')
   }
 }
   
